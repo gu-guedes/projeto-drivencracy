13badba46a42a964c44ddb250faf403b52dc4997
gu-guedes <guguedes2020@gmail.com>
2023-05-08 12:58:50 -0300

    mudanças no .env

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 import express from "express"
 import cors from "cors"
 import router from "./routes/index.route.js"
+import dotenv from "dotenv"
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(router)
+dotenv.config()
 
-const PORT = 5000
-app.listen(PORT, () => console.log(`servidor rodando na porta ${PORT}`))
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+	console.log(`Servidor rodando na porta ${port}`)
+
+})
